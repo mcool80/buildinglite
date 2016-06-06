e26b6ab1e586bbd17983345264ce813635095253
Markus Svensson <markus@celit.se>
2016-06-06 17:40:02 +0200

    Added change of user_type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,6 +60,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:password, :email, :last_name, :first_name, :mobile, :phone, :apartment_id, :password_confirmation)
+      params.require(:user).permit(:password, :email, :last_name, :first_name, :mobile, :phone, :apartment_id, :password_confirmation, :user_type)
     end
 end
